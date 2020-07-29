5d6626097ac8853fd7dd9a29a4a8860d4f9fcfa9
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-07-29 16:39:24 +0300

    [DocsLayout] created [package] added credentials

---
+++ src/lib/layout/DocsLayout.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import clsx from 'clsx';
+import {useTheme} from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import {useStyles} from './styles'
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {ChangeRouteProvider} from "routing-manager";
+
+function DocsLayout({pages = null, menu = null}) {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(true);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline/>
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={clsx(classes.menuButton, open && classes.hide)}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" noWrap>
+                        MUI Flexible Table Wiki
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="left"
+                open={open}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon/> : <ChevronRightIcon/>}
+                    </IconButton>
+                </div>
+                <Divider/>
+                {menu}
+            </Drawer>
+            <main
+                className={clsx(classes.content, {
+                    [classes.contentShift]: open,
+                })}
+            >
+                <div className={classes.drawerHeader}/>
+                <Switch>
+                    {pages}
+                </Switch>
+            </main>
+        </div>
+    );
+}
+
+function PagesSwitch() {
+    return (
+        <Switch>
+            <Route path={'/home'}>
+
+            </Route>
+            <Route path={'/flexible-table-api'}>
+            </Route>
+            {/*<Redirect from={'/'} to={'/home'}/>*/}
+        </Switch>
+    );
+}
+
+export default function DocsLayoutProviders() {
+    return (
+        <Router>
+            <ChangeRouteProvider routeMask={':page'}>
+                <App/>
+            </ChangeRouteProvider>
+        </Router>
+    );
+}
---
+++ src/lib/layout/styles.js
@@ -0,0 +1,71 @@
+/*
+ * Author: Danil Andrieiev | danssg08@gmail.com |  https://github.com/DanilAndreev
+ *  Copyright (c) 2020.
+ */
+
+import {makeStyles} from "@material-ui/core/styles";
+import {blue} from "@material-ui/core/colors";
+const drawerWidth = 240;
+
+export const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+    },
+    pageActive: {
+        color: blue[300],
+    },
+    nested: {
+        paddingLeft: theme.spacing(4),
+    },
+}));
