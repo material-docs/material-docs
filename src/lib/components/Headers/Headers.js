8c075868a5467f51190fe2801c1aabd5d5940f66
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-08-03 14:51:34 +0300

    [Headers] added tags to headers

--- src/lib/components/Headers/Headers.js
+++ src/lib/components/Headers/Headers.js
@@ -7,32 +7,43 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import createRouteFromName from "../../utils/createRouteFromName";
+import {useStyles} from "./styles";
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import clsx from "clsx";
 
-export function Tagable({children, systemOnTag, noTag=false, variant}) {
-    const id = typeof children === "string" && createRouteFromName(children);
+export function Tagable({children, systemOnTag, noTag = false, variant, style, className, ...props}) {
+    const classes = {...useStyles(), ...props.classes};
+    const id = props.id || (typeof children === "string" && createRouteFromName(children));
 
     React.useEffect(() => {
-        !noTag && typeof systemOnTag === "function" &&  systemOnTag(prev => ({...prev, [id]: String(children)}));
+        !noTag && typeof systemOnTag === "function" && systemOnTag(prev => ({...prev, [id]: String(children)}));
         return () => !noTag && typeof systemOnTag === "function" && systemOnTag(prev => ({...prev, [id]: undefined}));
     }, [children]);
 
     return (
-        <Typography variant={variant} id={!noTag && id}>
-            {children}
-            <Divider />
-        </Typography>
+        <div className={clsx(classes.root, classes.anchor, className)} id={!noTag && id} style={style}>
+            <div className={classes.container}>
+                <Typography variant={variant} >
+                    {children}
+                    <Divider/>
+                </Typography>
+                <a href={`#${id}`} className={classes.tagHook}>
+                    <LocalOfferIcon />
+                </a>
+            </div>
+        </div>
     );
 }
 
-export function H1({children, systemOnTag, noTag=false}) {
+export function H1({children, systemOnTag, noTag = false}) {
     return <Tagable systemOnTag={systemOnTag} variant={"h2"} noTag={noTag}>{children}</Tagable>
 }
 
-export function H2({children, systemOnTag, noTag=false}) {
+export function H2({children, systemOnTag, noTag = false}) {
     return <Tagable systemOnTag={systemOnTag} variant={"h3"} noTag={noTag}>{children}</Tagable>
 }
 
-export function H3({children, systemOnTag, noTag=false}) {
+export function H3({children, systemOnTag, noTag = false}) {
     return <Tagable systemOnTag={systemOnTag} variant={"h4"} noTag={noTag}>{children}</Tagable>
 }
 
---
+++ src/lib/components/Headers/styles.js
@@ -0,0 +1,37 @@
+/*
+ * Author: Danil Andrieiev | danssg08@gmail.com |  https://github.com/DanilAndreev
+ *  Copyright (c) 2020.
+ */
+
+import {makeStyles} from "@material-ui/core/styles";
+import {grey} from "@material-ui/core/colors";
+
+export const useStyles = makeStyles((theme) => ({
+    root: {},
+    container: {
+        position: "relative",
+    },
+    tagHook: {
+        position: "absolute",
+        left: -25,
+        top: "10%",
+        color: grey[600],
+        display: "none",
+        padding: 20,
+        paddingTop: 0,
+        paddingLeft: 0,
+        "&:hover": {
+            color: grey[700],
+            display: "block",
+        },
+    },
+    anchor: {
+        "&:target": {
+            paddingTop: 64,
+            marginTop: -64,
+        },
+        "&:hover $tagHook": {
+            display: "block",
+        }
+    }
+}));
