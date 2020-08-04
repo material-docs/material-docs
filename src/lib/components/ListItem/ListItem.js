05850de51f4a7efe16409d81e9d72eb44f9b7c42
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-08-04 17:06:22 +0300

    small changes

--- src/lib/components/ListItem/ListItem.js
+++ src/lib/components/ListItem/ListItem.js
@@ -2,15 +2,32 @@ import React from "react";
 import {ListItem as MUIListItem} from "@material-ui/core";
 import ListItemContained from "../ListItemContained";
 import List from "@material-ui/core/List";
+import {useStyles} from "./styles";
+import clsx from "clsx";
 
-function ListItem({children, ...props}, ref) {
+function ListItem({children, type = "circle", ...props}, ref) {
+    const classes = useStyles();
     const containers = React.Children.map(children, child => child.type === ListItemContained ? child : null);
     if (containers.length > 1) console.error("MaterialDocs: List item can contain only one ListItemContained element");
 
     const containedItems = containers[0] && containers[0].props && containers[0].props.children;
     const content = React.Children.map(children, child => child.type === ListItemContained ? null : child);
+
+    let listTypeClass = classes.typeCircle;
+    switch (type) {
+        case "square":
+            listTypeClass = classes.typeSquare;
+            break;
+        case "upper-roman":
+            listTypeClass = classes.typeRoman;
+            break;
+        case "lower-alpha":
+            listTypeClass = classes.typeAlpha;
+            break;
+    }
+
     return (
-        <MUIListItem {...props} ref={ref}>
+        <MUIListItem {...props} ref={ref} className={clsx(classes.root, listTypeClass)}>
             <div>
                 {containedItems &&
                 <div>
---
+++ src/lib/components/ListItem/styles.js
@@ -0,0 +1,23 @@
+/*
+ * Author: Danil Andrieiev | danssg08@gmail.com |  https://github.com/DanilAndreev
+ *  Copyright (c) 2020.
+ */
+
+import {makeStyles} from "@material-ui/core/styles";
+import {grey} from "@material-ui/core/colors";
+
+export const useStyles = makeStyles((theme) => ({
+    root: {},
+    typeCircle: {
+        listStyleType: "circle",
+    },
+    typeSquare: {
+        listStyleType: "square",
+    },
+    typeRoman: {
+        listStyleType: "upper-roman",
+    },
+    typeAlpha: {
+        listStyleType: "lower-alpha",
+    },
+}));
