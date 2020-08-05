02491d99c9cad8ad740ada9c9da3cf56d89dac3b
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-08-05 16:58:30 +0300

    [Example] small changes

--- src/example/Pages/GettingStarted/OverviewPage.js
+++ src/example/Pages/GettingStarted/OverviewPage.js
@@ -18,6 +18,7 @@ export default function InstallationPage() {
             <H1>Material Docs</H1>
             <H3>Simple react framework for creating documentation</H3>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. A distinctio eaque esse fugiat itaque laborum, ratione voluptas! Aut consequuntur dolorem doloremque dolores enim non odio odit placeat praesentium repudiandae? A autem beatae commodi consequuntur dignissimos dolore dolores ducimus eius enim et excepturi explicabo fugit impedit in ipsa iste maxime modi nam nihil nisi obcaecati officiis omnis pariatur perspiciatis possimus praesentium quibusdam quis quisquam quos rem saepe similique sunt totam velit veritatis voluptate, voluptates. Aliquid aspernatur, dolores esse illum maxime modi perferendis quae. Aliquid aspernatur corporis distinctio expedita laborum molestiae nisi omnis rem, sit velit vitae voluptatibus voluptatum. Nesciunt, odio voluptas.
+            <H3>Nice picture, yeah!</H3>
             <Image src={image1}>
                 Caption of image
             </Image>
