a1e11469d9940ccfe8149998a4a32714b169dfe7
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-08-19 22:08:47 +0300

    [Docs] Some changes.

---
+++ docs/precache-manifest.48b06f76d50240b13f6ceddbc99d74b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a174b074974d16cf947d0d3d95b384a",
+    "url": "/material-docs/index.html"
+  },
+  {
+    "revision": "f3630ca2fe9197e69955",
+    "url": "/material-docs/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "5d3567dd28d2bc617cf3",
+    "url": "/material-docs/static/js/2.d412fae4.chunk.js"
+  },
+  {
+    "revision": "c9dd85361cb9cef9841c7e9bd1147de5",
+    "url": "/material-docs/static/js/2.d412fae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3630ca2fe9197e69955",
+    "url": "/material-docs/static/js/main.c1b6b79d.chunk.js"
+  },
+  {
+    "revision": "481163103c8b619c217c",
+    "url": "/material-docs/static/js/runtime-main.2367d2a7.js"
+  },
+  {
+    "revision": "8009a378b8b56351c7b7ba197a9a08f9",
+    "url": "/material-docs/static/media/image1.8009a378.jpg"
+  }
+]);
