e67a2ca24577619cb94fb49c548a9c2487d345e8
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-08-20 19:03:11 +0300

    [Markdown] Cleaned up. [Docs] changed imports to @danilandreev/material-docs

--- src/docs-app/Documentation.js
+++ src/docs-app/Documentation.js
@@ -4,29 +4,27 @@
  */
 
 import React from "react";
-import DocsLayout from "../lib/layout/DocsLayout";
-import DocsPages from "../lib/components/DocsPages";
-import PagesGroup from "../lib/components/PagesGroup/PagesGroup";
 import DocsPageDemo from "./pages/Components/DocsPageDemo";
 import DocsPageAPI from "./pages/APIs/DocsPageAPI";
 
+import {DocsLayout, DocsPages, PagesGroup} from "@danilandreev/material-docs";
+
 import RU from "./locale/RU.json";
 import EN from "./locale/EN.json";
-import useLang from "../lib/hooks/useLang/useLang";
 
 
 export default function Documentation() {
-    return(
+    return (
         <DocsLayout autoMenu defaultLang={EN} langs={[EN, RU]} basename={process.env.PUBLIC_URL}>
             <DocsPages>
                 <PagesGroup name={"Getting started"}>
 
                 </PagesGroup>
                 <PagesGroup name={"Components"}>
-                    <DocsPageDemo />
+                    <DocsPageDemo/>
                 </PagesGroup>
                 <PagesGroup name={"APIs"}>
-                    <DocsPageAPI />
+                    <DocsPageAPI/>
                 </PagesGroup>
             </DocsPages>
         </DocsLayout>
