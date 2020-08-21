8e5ce33d5adfc73f4e84480c04dd19653ed97937
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-08-21 12:40:11 +0300

    [Docs] Added new build.

---
+++ docs/precache-manifest.1fb5c68fc16f07bd7e72e6f11219979a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896cbeb56a8f2acfb821c806609e493d",
+    "url": "/material-docs/index.html"
+  },
+  {
+    "revision": "aa83942ac5c907240d98",
+    "url": "/material-docs/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "7433956a03fb53511940",
+    "url": "/material-docs/static/js/2.40187d66.chunk.js"
+  },
+  {
+    "revision": "c9dd85361cb9cef9841c7e9bd1147de5",
+    "url": "/material-docs/static/js/2.40187d66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa83942ac5c907240d98",
+    "url": "/material-docs/static/js/main.4824abd0.chunk.js"
+  },
+  {
+    "revision": "ee6e2cd02f908079f9d5",
+    "url": "/material-docs/static/js/runtime-main.f61c784e.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/material-docs/precache-manifest.37bae980e33eacacf1c547995dbbb3ae.js"
+  "/material-docs/precache-manifest.1fb5c68fc16f07bd7e72e6f11219979a.js"
 );
 
 self.addEventListener('message', (event) => {
