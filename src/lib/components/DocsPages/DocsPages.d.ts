dd62b262688a16fbcfb73db4a701be8fe7347553
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-08-25 20:47:44 +0300

    [Components] Small changes in folders layout. [npm-publish] Changed trigger to on push.

---
+++ src/lib/components/DocsPages/DocsPages.d.ts
@@ -0,0 +1,21 @@
+/*
+ * Author: Andrieiev Danil |  danssg08@gmail.com | https://github.com/DanilAndreev
+ * Copyright (C) 2020.
+ */
+
+// @ts-ignore
+import React from "react";
+import Containerable from "../../interfaces/Containerable";
+
+export interface DocsPagesProps
+    extends Containerable {
+    /**
+     * children - children components, normally - DocsPage or PagesGroup.
+     * @type any
+     */
+    children?: any;
+}
+
+export default class DocsPages extends React.Component<DocsPagesProps, any> {
+    render(): JSX.Element;
+}
