601d91e38571069c332c91c09645231b58c80478
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-08-26 17:08:42 +0300

    [DocsLayout] Optimized design for mobiles.

---
+++ src/lib/components/Table/styles.js
@@ -0,0 +1,15 @@
+/*
+ * Author: Andrieiev Danil |  danssg08@gmail.com | https://github.com/DanilAndreev
+ * Copyright (C) 2020.
+ */
+
+import {makeStyles} from "@material-ui/core/styles";
+import {grey} from "@material-ui/core/colors";
+
+//TODO:  finish tables horizontal scrolling
+export const useStyles = makeStyles((theme) => ({
+    container: {
+        width: "100%",
+        overflowX: "auto",
+    },
+}));
