5f3f2767a6f265aa301c40b4c320178d2f163141
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-08-28 16:41:47 +0300

    [Theme] Added DefaultTheme.

---
+++ src/lib/theme/DefaultTheme.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Andrieiev Danil |  danssg08@gmail.com | https://github.com/DanilAndreev
+ * Copyright (C) 2020.
+ */
+
+import { createMuiTheme } from '@material-ui/core/styles';
+import blue from "@material-ui/core/colors/blue";
+import red from "@material-ui/core/colors/red";
+
+const DefaultTheme = createMuiTheme({
+    palette: {
+        primary: {
+            main: blue[700],
+        },
+        secondary: {
+            main: red[600],
+        },
+    },
+});
+
+export default DefaultTheme;
