7ad67b43453b1588601c720c38bb39701c8efa92
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-08-31 09:02:57 +0300

    [DocsLayout] Added new docs build. (issue #106)

---
+++ docs/precache-manifest.c782e1eb23d926961b7639c0e849670c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94e521c0e13f248d85b0fd027ec28cb8",
+    "url": "/material-docs/index.html"
+  },
+  {
+    "revision": "c27d8cef0904b0d2435a",
+    "url": "/material-docs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ea9618fda26d322b96d5",
+    "url": "/material-docs/static/js/2.0e149afd.chunk.js"
+  },
+  {
+    "revision": "c9dd85361cb9cef9841c7e9bd1147de5",
+    "url": "/material-docs/static/js/2.0e149afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c27d8cef0904b0d2435a",
+    "url": "/material-docs/static/js/main.56d3f8eb.chunk.js"
+  },
+  {
+    "revision": "ee6e2cd02f908079f9d5",
+    "url": "/material-docs/static/js/runtime-main.f61c784e.js"
+  }
+]);
