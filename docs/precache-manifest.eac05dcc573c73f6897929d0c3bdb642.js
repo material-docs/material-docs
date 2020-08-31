37618ceb175c82dcae755888fe0b328870f9b86c
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-08-31 11:21:45 +0300

    [Documentation] Added new docs version.

---
+++ docs/precache-manifest.eac05dcc573c73f6897929d0c3bdb642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739125a6d46cb533473987f3e76f4ea6",
+    "url": "/material-docs/index.html"
+  },
+  {
+    "revision": "10840abd708046d1eb39",
+    "url": "/material-docs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e221a8a1ec8324bc1055",
+    "url": "/material-docs/static/js/2.4ea1cddc.chunk.js"
+  },
+  {
+    "revision": "c9dd85361cb9cef9841c7e9bd1147de5",
+    "url": "/material-docs/static/js/2.4ea1cddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10840abd708046d1eb39",
+    "url": "/material-docs/static/js/main.61857dba.chunk.js"
+  },
+  {
+    "revision": "ee6e2cd02f908079f9d5",
+    "url": "/material-docs/static/js/runtime-main.f61c784e.js"
+  }
+]);
