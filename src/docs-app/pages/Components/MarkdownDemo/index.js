81b5580c360afa501ea4323fea4afa5a30f0c6ad
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-09-08 21:51:07 +0300

    [Documentation] Added Markdown demo.

--- src/docs-app/pages/Components/MarkdownDemo/index.js
+++ src/docs-app/pages/Components/MarkdownDemo/index.js
@@ -5,13 +5,22 @@
 
 import DocsPage from "@danilandreev/material-docs/components/DocsPage/DocsPage";
 import React from "react";
-import {H1, H2, H3} from "@danilandreev/material-docs/components/Headers";
+import {H1, H2, H3, H4, H5} from "@danilandreev/material-docs/components/Headers";
 import DemoWithCode from "@danilandreev/material-docs/components/DemoWithCode/DemoWithCode";
 import MarkdownOverview from "../../../examples/Markdown/MarkdownOverview";
 import {TaggingContext} from "@danilandreev/material-docs/hooks/useTags";
 import Box from "@material-ui/core/Box";
 import Markdown from "@danilandreev/material-docs/components/Markdown/Markdown";
 import useLang from "@danilandreev/material-docs/hooks/useLang/useLang";
+import Table from "@danilandreev/material-docs/components/Table/Table";
+import {TableCell, TableBody, TableHead, TableRow} from "@danilandreev/material-docs";
+import Code from "@danilandreev/material-docs/components/Code/Code";
+import CodeSpan from "@danilandreev/material-docs/components/CodeSpan/CodeSpan";
+import Block from "@danilandreev/material-docs/components/Block/Block";
+import Bold from "@danilandreev/material-docs/components/Bold/Bold";
+import ExpansionCodeMD from "../../../examples/Markdown/ExpansionCodeMD";
+import DemoWithCodeMD from "../../../examples/Markdown/DemoWithCodeMD";
+import CodeMD from "../../../examples/Markdown/CodeMD";
 
 const MarkdownOverviewCode = `
 import React from "react";
@@ -34,8 +43,110 @@ export default function MarkdownOverview() {
         </Markdown>
     );
 }
+`.trim();
+
+
+const expansionCodeBlockExample = `
+import React from "react";
+import Markdown from "@danilandreev/material-docs/components/Markdown";
+
+const markdown = \`
+\\\`\\\`\\\`{"type": "expansion-code", "theme": "darcula", "name": "ExpansionCode in markdown"}
+import React from "react";
+
+function test() {
+    console.log("I am function in expansion code element.");
+    return null;
+}
+\\\`\\\`\\\`
+\`.trim();
+
+export default function MarkdownOverview() {
+    return (
+        <Markdown>
+            {markdown}
+        </Markdown>
+    );
+}
+`.trim();
+
+const demoWithCodeBlockExample = `
+import React from "react";
+import Markdown from "@danilandreev/material-docs/components/Markdown";
+
+function HelloWorld() {
+    return (
+        <div>
+            <h1>Hello darkness my old friend</h1>
+        </div>
+    );
+}
+
+const markdown = \`
+\\\`\\\`\\\`
+{
+    "type"
+:
+    "demo-with-code", "theme"
+:
+    "darcula", "name"
+:
+    "DemoWithCode in markdown", "demo"
+:
+    "exampleFunction"
+}
+import React from "react";
+
+function HelloWorld() {
+    return (
+        <div>
+            <h1>Hello darkness my old friend</h1>
+        </div>
+    );
+}
+\\\`\\\`\\\`
+\`.trim();
+
+export default function DemoWithCodeMD() {
+    return (
+        <Markdown data={{exampleFunction: <HelloWorld/>}}>
+            {markdown}
+        </Markdown>
+    );
+}
+`.trim();
+
+const codeBlockExample = `
+import React from "react";
+import Markdown from "@danilandreev/material-docs/components/Markdown";
+
+const markdown = \`
+\\\`\\\`\\\`{"type": "code", "theme": "darcula"}
+import React from "react";
+
+function test() {
+    console.log("I am function in code element.");
+    return null;
+}
+\\\`\\\`\\\`
+\`.trim();
+
+export default function CodeMD() {
+    return (
+        <Markdown>
+            {markdown}
+        </Markdown>
+    );
+}
 
 `.trim();
+
+
+const codeBlockExamples = {
+    "expansion-code": {code: expansionCodeBlockExample, demo: <ExpansionCodeMD/>},
+    "demo-with-code": {code: demoWithCodeBlockExample, demo: <DemoWithCodeMD/>},
+    "code": {code: codeBlockExample, demo: <CodeMD/>},
+}
 
 export default function MarkdownDemo() {
     const {lang} = useLang();
@@ -59,13 +170,58 @@ export default function MarkdownDemo() {
                 name={locale.overviewExample}
             >
                 <Box p={2}>
-                    <TaggingContext.Provider value={{setTag: () => {}, removeTag: () => {}, tags: {}}}>
+                    <TaggingContext.Provider value={{
+                        setTag: () => {
+                        }, removeTag: () => {
+                        }, tags: {}
+                    }}>
                         <MarkdownOverview/>
                     </TaggingContext.Provider>
                 </Box>
             </DemoWithCode>
+            <H2>{locale.moreInfo}</H2>
+            <Markdown>{locale.moreInfoDetails}</Markdown>
             <H2>{locale.deepIntegration}</H2>
             <Markdown>{locale.infoBlock2}</Markdown>
+            {locale.codeComponents.map(item => {
+                const demo = codeBlockExamples[item.key];
+                return (
+                    <React.Fragment key={item.key}>
+                        <H3 noTag={false}>{item.name}</H3>
+                        {item.description && <Markdown>{item.description}</Markdown>}
+                        {demo &&
+                        <DemoWithCode
+                            code={demo.code}
+                            theme={"darcula"}
+                            paperContainer
+                        >
+                            <Box p={2}>
+                                <TaggingContext.Provider value={{setTag: () => {}, removeTag: () => {}, tags: {}}}>
+                                    {demo.demo}
+                                </TaggingContext.Provider>
+                            </Box>
+                        </DemoWithCode>
+                        }
+                        <Block><Bold>Key</Bold> - <CodeSpan>{item.key}</CodeSpan></Block>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Name</TableCell>
+                                    <TableCell>Description</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {item.params.map(param => (
+                                    <TableRow key={param.name}>
+                                        <TableCell><CodeSpan>{param.name}</CodeSpan></TableCell>
+                                        <TableCell>{param.description}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </React.Fragment>
+                )
+            })}
         </DocsPage>
     );
 }
