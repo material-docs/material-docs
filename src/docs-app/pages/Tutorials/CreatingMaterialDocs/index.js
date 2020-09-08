c9c08d1f789f09917b79de61244e7773ccc8ce98
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-09-08 13:34:11 +0300

    [Documentation] CreatingMaterialDocs tutorial.

---
+++ src/docs-app/pages/Tutorials/CreatingMaterialDocs/index.js
@@ -0,0 +1,19 @@
+/*
+ * Author: Andrieiev Danil |  danssg08@gmail.com | https://github.com/DanilAndreev
+ * Copyright (C) 2020.
+ */
+
+import React from "react";
+import DocsPage from "@danilandreev/material-docs/components/DocsPage/DocsPage";
+
+export default function CreatingMaterialDocs() {
+    return (
+        <DocsPage
+            name={"Creating Material Docs"}
+            searchTags={["create", "material", "docs", "tutorial", "faq", "manual"]}
+            searchLabel={"Creating Material Docs"}
+        >
+
+        </DocsPage>
+    );
+}
